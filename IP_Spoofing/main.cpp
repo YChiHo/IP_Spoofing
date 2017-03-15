b37ffb2413e2a5f41b5ed90c455f65afc28e84d6
Ychiho <chiho2736@gmail.com>
2017-03-16 01:44:12 +0900

    finish lib

--- IP_Spoofing/main.cpp
+++ IP_Spoofing/main.cpp
@@ -1,16 +1,20 @@
-#include<tins/tins.h>
+#define TINS_STATIC
 #include<iostream>
+#include<tins/tins.h>
+#include<tins/ethernetII.h>
 #include<WinSock2.h>
-#define HAVE_REMOTE
-#define WPCAP
-#include<string>
+#include<pcap.h>
 #include<IPHlpApi.h>
 
-using namespace std;
+using std::cout;
+using std::runtime_error;
+using std::endl;
 using namespace Tins;
 
 #pragma comment(lib, "ws2_32.lib")		//winsock2
+#pragma comment(lib, "wpcap.lib")		//winpcap
 #pragma comment(lib, "iphlpapi.lib")	//get mac
+#pragma comment(lib, "tins.lib")
 
 void init() {
 #ifdef _WIN32_
@@ -19,7 +23,18 @@ void init() {
 #endif // _WIN32_
 }
 
-int main() {
+void do_ip_spoofing(NetworkInterface iface, IPv4Address target, IPv4Address gateway, const NetworkInterface::Info& info) {
 
+}
+
+int main(int argc, char* argv[]) {
+	if (argc != 3) {
+		cout << "ips <Target> <Gateway>" << endl;
+		return -1;
+	}
+
+	PacketSender sender;
+
+	IPv4Address target_ip = IPv4Address(argv[2]);
 	return 0;
 }
